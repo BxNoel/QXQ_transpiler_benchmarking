23a504c58d48abaa658a4b8694923a594cf294ef
bxnoel <noelnegron03@gmail.com>
2023-07-13 11:50:28 -0400

    adding circuit_depth...

---
+++ random_ciruits.py
@@ -0,0 +1,10 @@
+#This file will be responsible for generating random Quantum Circuits
+import qiskit
+from qiskit.circuit.random import random_circuit
+import random
+
+noel = randint(2,3)
+print(noel)
+
+def generate_random_circuits(num_circuits, num_qubtis, max_depth):
+    return None
