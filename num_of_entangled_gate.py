d9fa70c77d150ca35f5cb6b03fcb56e08f8e31d8
victor osunji <70077058+victorro7@users.noreply.github.com>
2023-07-20 14:17:32 -0400

    Update num_of_entangled_gate.py
    created find_num_entagled_gates function

--- num_of_entangled_gate.py
+++ num_of_entangled_gate.py
@@ -21,46 +21,68 @@ def file_reader(file_path):
 
     return circuits
 
+def find_num_entagled_gates(circuits):
+
+    # Select the backend (FakeSherbrooke) for transpilation
+    backend = FakeSherbrooke()
+    # count the entangled gates, and store the results before transpiling
+    entangled_gate_counts = []
+    for circuit in circuits:
+        entangled_gate_count = sum([1 for gate in circuit if gate[0].name in entangling_gates_type])
+        entangled_gate_counts.append(entangled_gate_count)
+
+    # Dictionary to store transpiled circuits for each optimization level
+    transpiled_circuits = {3: [],
+                        2: [],
+                        1: []} 
+
+    for circuit in circuits:
+        for opt_level in [3, 2, 1]:
+            transpiled_circuit = transpile(circuit, backend, optimization_level=opt_level)
+            transpiled_circuits[opt_level].append(transpiled_circuit)
+
+
+    # Find entangling gates in transpiled circuits
+    entangled_gates = []
+    def find_entangled_qubits(transpiled_circuits):
+        for gate in transpiled_circuits:
+            if gate in entangling_gates_type:
+                entangled_gates.append(transpiled_circuits[gate])
+        
+        return entangled_gates
+
+    for circuit in transpiled_circuits[3]:
+        entangling_gates = find_entangled_qubits(circuit.count_ops())
+
+    # Plot the number of entangled gates before and after transpile to compare (lvl 3)
+    plt.plot(range(1, len(transpiled_circuits[3]) + 1), entangling_gates, marker='o')
+    plt.plot(range(1, len(circuits) + 1), entangled_gate_counts, marker='o')
+    plt.xlabel('Circuit')
+    plt.ylabel('Entangled Gate Count')
+    plt.title('Number of Entangled Gates (normal vs transpiled v3)')
+    plt.xticks(range(1, len(transpiled_circuits[3]) + 1))
+    plt.show()
+
+    # Plot the number of entangled gates before and after transpile to compare (lvl 2)
+    plt.plot(range(1, len(transpiled_circuits[2]) + 1), entangling_gates, marker='o')
+    plt.plot(range(1, len(circuits) + 1), entangled_gate_counts, marker='o')
+    plt.xlabel('Circuit')
+    plt.ylabel('Entangled Gate Count')
+    plt.title('Number of Entangled Gates (normal vs transpiled v2)')
+    plt.xticks(range(1, len(transpiled_circuits[3]) + 1))
+    plt.show()
+
+    # Plot the number of entangled gates before and after transpile to compare (lvl 1)
+    plt.plot(range(1, len(transpiled_circuits[1]) + 1), entangling_gates, marker='o')
+    plt.plot(range(1, len(circuits) + 1), entangled_gate_counts, marker='o')
+    plt.xlabel('Circuit')
+    plt.ylabel('Entangled Gate Count')
+    plt.title('Number of Entangled Gates (normal vs transpiled v1)')
+    plt.xticks(range(1, len(transpiled_circuits[3]) + 1))
+    plt.show()
+
+
 # Read circuits from files
 circuits = file_reader("test_su2circ")
-
-# Select the backend (FakeSherbrooke) for transpilation
-backend = FakeSherbrooke()
-
-# count the entangled gates, and store the results before transpiling
-entangled_gate_counts = []
-for circuit in circuits:
-    entangled_gate_count = sum([1 for gate in circuit if gate[0].name in entangling_gates_type])
-    entangled_gate_counts.append(entangled_gate_count)
-
-# Dictionary to store transpiled circuits for each optimization level
-transpiled_circuits = {3: [],
-                       2: [],
-                       1: []} 
-
-for circuit in circuits:
-    for opt_level in [3, 2, 1]:
-        transpiled_circuit = transpile(circuit, backend, optimization_level=opt_level)
-        transpiled_circuits[opt_level].append(transpiled_circuit)
-
-
-# Find entangling gates in transpiled circuits
-entagling_gates = []
-def find_entangled_qubits(transpiled_circuits):
-    for gate in transpiled_circuits:
-        if gate in entangling_gates_type:
-            entagling_gates.append(transpiled_circuits[gate])
-    
-    return entagling_gates
-
-for circuit in transpiled_circuits[3]:
-    entangling_gates = find_entangled_qubits(circuit.count_ops())
-
-# Plot the number of entangled gates before and after transpile to compare
-plt.plot(range(1, len(transpiled_circuits[3]) + 1), entangling_gates, marker='o')
-plt.plot(range(1, len(circuits) + 1), entangled_gate_counts, marker='o')
-plt.xlabel('Circuit')
-plt.ylabel('Entangled Gate Count')
-plt.title('Number of Entangled Gates (normal vs transpiled v3)')
-plt.xticks(range(1, len(transpiled_circuits[3]) + 1))
-plt.show()
+#find number of entangled gates
+find_num_entagled_gates(circuits)
