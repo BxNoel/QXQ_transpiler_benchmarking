983f27014793dcb01d7f1e9cc7bf6d8c19d53032
Cat <98866864+Procatv@users.noreply.github.com>
2023-08-11 10:10:24 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,25 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+setup(
+    name='QXQ_transpiler_benchmarking',
+    version='0.0.1',
+    description='QXQ Benchmarking tool',
+    url='https://github.com/BxNoel/QXQ_transpiler_benchmarking',
+    author=[
+        {'name': 'Noel Negron', 'email': 'nmn2127@columbia.edu'},
+        {'name': 'Guadalupe Cantera', 'email': 'Guadalupe_cantera@brown.edu'},
+        {'name': 'Catherine Lozano', 'email': 'Catherinelibra17@gmail.com'},
+        {'name': 'Victor Osunji, 'email': 'victorosunij@gmail.com'},
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",  # Updated license information
+        "Operating System :: OS Independent",
+    ],
+    keywords=["MQT", "quantum computing", "benchmarking", "performance", "testing"],
+    license="Apache License 2.0",  # Updated license information
+    python_requires=">=3.9",
+    packages=find_packages(),
+    install_requires=['qiskit', 'numpy', 'matplotlib'],
+    include_package_data=True,
+)
